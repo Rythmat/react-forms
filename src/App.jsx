a0296ce8f6c612111b60082d4771e4b71c00f42c
Rythmat <teotaniwha@gmail.com>
2025-02-06 12:44:08 -0700

    fix file heirarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState} from "react"
+
+import Authenticate from "./components/Authenticate.jsx"
+import SignUpForm from "./components/SignUpForm.jsx"
+
 function App() {
+  const [token,setToken] = useState(null);
+  
    return (
     <>
       <h1>Hello</h1>
+      <SignUpForm setToken={setToken}/>
+      <Authenticate token={token}/>
     </>
   )
 }
