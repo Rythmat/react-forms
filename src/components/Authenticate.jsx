4fffc68824f587f4a563f1a31fc5b398e4eb520c
Rythmat <teotaniwha@gmail.com>
2025-02-06 17:07:15 -0700

    Wrapping Up

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react"
 
-const Authenticate = ({token}) => {
-  const [error,setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
-
+const Authenticate = ({token, setError, setSuccessMessage,setUser}) => {
+  //function to handle the clicking of the authenticate button
   const handleClick = async(event) => {
+    //get our authenication details
     try {
       const response = await fetch("https://fsa-jwt-practice.herokuapp.com/authenticate",
         {
@@ -16,17 +15,25 @@ const Authenticate = ({token}) => {
         }
       )
       const jsonObj = await response.json();
-      setSuccessMessage(jsonObj.message);
+      //if the authentication was complete, then display the success message
+      if(jsonObj.success){
+        setSuccessMessage(jsonObj.message);
+        setUser(jsonObj.data.username);
+        setError(null);
+        
+      //otherwise throw an error to be caught  
+      }else{
+        throw new Error(jsonObj.message);
+      }
+      
     } catch (error) {
-      setError(error.message);
+      setError('Error: ' +error.message);
     }
   }
 
   return (
     <>
       <h2>Authenticate</h2>
-      {error && <p>{error}</p>}
-      {successMessage && <p>{successMessage}</p>}
       <button onClick={handleClick}>Authenticate Token</button>
     </>
   )
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,14 +1,57 @@
 import { useState } from "react"
 
-const SignUpForm = ({setToken}) => { 
+const SignUpForm = ({setToken, setError}) => {
+  //State variables 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
 
+  //Function to check password validity based on regular expressions
+  function passwordValidate(pw) {
+    var re = {
+        'capital' : /[A-Z]/,
+        'digit'   : /[0-9]/,
+        'special'  : /\W/,
+        'full'    : /.{7,}$/
+    };
+    return re.capital .test(pw) && 
+           re.digit   .test(pw) && 
+           re.special .test(pw) && 
+           re.full    .test(pw);
+    }
+
+  //Button handler for the form submit
   const handleSubmit = async(event) => {
+    //Prevents the page resetting on submit by preventing the default action
     event.preventDefault();
+
+    //Regular expression to match a good username
+    const validUsername = new RegExp('^[a-zA-Z0-9-_]{7,}$');
+    //Empty array to hold the errors about the submit
+    const badDeets = [];
+    //reset the error to prevent looping
+    setError(null);
+
+    //for bad usernames
+    if(!validUsername.test(username)){
+      badDeets.push('Your username must be longer than 7 alphanumeric only characters. ')
+    }
+    //for bad passwords
+    if(!passwordValidate(password)){
+      badDeets.push('Your password must contain 8 characters and at least one special character, one letter, and one number.');
+    }
+    //for empty inputs
+    if(username==='' || password===''){
+      setError('Please enter both a username and a password!');
+      return;
+    }
+    //If there is an issue with the form
+    if(badDeets.length>0){
+      setError('Invalid! ' + badDeets.join(''));
+      return;
+    }
+
+    //If the form was valid, then post it in to the API
     try {
-      //Attempting the user input into the API
       const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
        {
           method: "POST",
@@ -23,10 +66,10 @@ const SignUpForm = ({setToken}) => {
     }
   }
 
+
   return (
     <>
       <h2>Sign Up</h2>
-      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:  <input type="text" placeholder="username" onChange={(event)=>{setUsername(event.target.value)}}/>
@@ -34,7 +77,6 @@ const SignUpForm = ({setToken}) => {
         <label>
           Password:  <input type="text" placeholder="password" onChange={(event)=>{setPassword(event.target.value)}}/>
         </label>
-        
         <button>Submit</button>
       </form>
     </>
